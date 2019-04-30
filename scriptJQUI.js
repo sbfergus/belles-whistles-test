ddb4b44546d19e669cf425b9d8c1836fde4710f0
unknown <sbferguson17@gmail.com>
2019-04-30 13:29:10 -0600

    jq ui

---
+++ scriptJQUI.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $("#img1").mouseover(function(e) {
+    $("#cart1").show("slide", {direction:"right"});
+      });     
+  $("#img2").mouseover(function(e) {
+    $("#cart2").show("slide", {direction:"right"});
+      });
+  $("#img3").mouseover(function(e) {
+    $("#cart3").show("slide", {direction:"right"});
+      });
+   $("#img4").mouseover(function(e) {
+     $("#cart4").show("slide", {direction:"right"});
+      });
+   $("#img5").mouseover(function(e) {
+     $("#cart5").show("slide", {direction:"right"});
+      });
+   $("#img6").mouseover(function(e) {
+     $("#cart6").show("slide", {direction:"right"});
+      });
+   $("#img1").mouseover(function(e) {
+    $("#title1").show("slide", {direction:"left"});
+      });
+  $("#img2").mouseover(function(e) {
+    $("#title2").show("slide", {direction:"left"});
+      });
+  $("#img3").mouseover(function(e) {
+    $("#title3").show("slide", {direction:"left"});
+      });
+  $("#img4").mouseover(function(e) {
+    $("#title4").show("slide", {direction:"left"});
+      });
+  $("#img5").mouseover(function(e) {
+    $("#title5").show("slide", {direction:"left"});
+      });
+  $("#img6").mouseover(function(e) {
+    $("#title6").show("slide", {direction:"left"});
+      });    
+     });
